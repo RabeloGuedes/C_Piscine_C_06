f64d354fb5fe16e73f4971160b39e1e9b6950832
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-03-14 14:29:36 +0000

    feat: Adds the folder 'ex02' and its file 'ft_rev_params.c'.

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42lisboa.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 13:57:23 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/03/14 14:29:06 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		while (argc > 1)
+		{
+			ft_print(argv[argc - 1]);
+			argc--;
+			ft_putchar('\n');
+		}
+	}
+	return (0);
+}
